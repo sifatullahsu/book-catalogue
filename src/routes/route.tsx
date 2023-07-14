faf4ff5fad7b17de43aeb29f26faf4bc5c02d141
sifatullahsu <personal.sifat@gmail.com>
2023-07-14 17:08:08 +0600

    project & router setup

---
+++ src/routes/route.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{}],
+  },
+]);
+
+export default routes;
