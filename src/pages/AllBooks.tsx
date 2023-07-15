ffcf3d68a41245f8f7d287b01a0ae3a889d53cb8
sifatullahsu <personal.sifat@gmail.com>
2023-07-15 14:41:13 +0600

    all book page

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,7 +1,25 @@
+import BookGrid from "../components/BookGrid";
+
 const AllBooks = () => {
   return (
     <>
-      <div>AllBooks</div>
+      <div className="container">
+        <div className="py-16">
+          <div className="text-center pb-8">
+            <h2 className="text-xl font-semibold">All Books</h2>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            <BookGrid />
+            <BookGrid />
+            <BookGrid />
+            <BookGrid />
+            <BookGrid />
+            <BookGrid />
+            <BookGrid />
+            <BookGrid />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
